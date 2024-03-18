1d94edb7a6613cc1025a68056559f14dc84f028a
Natalie Russell <natprussell@gmail.com>
2024-03-18 09:48:16 -0500

    added rows and columns to button placement

--- 01_ Intro_GUIs.py
+++ 01_ Intro_GUIs.py
@@ -6,11 +6,11 @@ root= Tk()
 btn1=ttk.Button(root)
 btn1.config(text= "Click here!")
 # can be used to add text or context to button
-btn1.grid()
+btn1.grid(row=0, column=0)
 # can use .grid  or .place  or .pack 
 
 btn2=ttk.Button(root, text= "This is button 2")
-btn2.grid()
+btn2.grid(row=1, column=2)
 # by adding ttk.button it creates a themed button 
 
 
