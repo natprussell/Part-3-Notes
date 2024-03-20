eab57507f0365dac22e784c03d859a67d386fd5e
Natalie Russell <natprussell@gmail.com>
2024-03-20 09:22:15 -0500

    addition of second button and frame

--- 04_ Frame Practice.py
+++ 04_ Frame Practice.py
@@ -3,14 +3,23 @@ from tkinter import ttk
 
 root= Tk()
 
-frame1= Frame(root, relief=SOLID, padx=10, pady=10)
-frame1.grid
+frame1= ttk.Frame(root, relief=SOLID, padding=(12,35))
+frame1.grid()
 
 btn1= ttk.Button(frame1, text= "Click here!")
 btn1.grid()
 
-btn2=ttk.Button(frame1, text= "Button 2")
+btn2= ttk.Button(frame1, text= "Button 2")
 btn2.grid()
+
+frame2= ttk.Frame(root, relief=SOLID, padding=(12,35))
+frame2.grid()
+
+btn3= ttk.Button(frame2, text= "Button 3")
+btn3.grid()
+
+btn4= ttk.Button(frame2, text= "Button 4")
+btn4.grid()
 
 
 root.mainloop()
