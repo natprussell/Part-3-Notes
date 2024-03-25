078b657261272355eed6024350a1aec8ba81b956
Natalie Russell <natprussell@gmail.com>
2024-03-25 09:42:23 -0500

    even more stuff with GUIs

--- Classes/03_GUIclass.py
+++ Classes/03_GUIclass.py
@@ -5,8 +5,17 @@ root= Tk()
 class GreetingApp:
     def __init__ (self, master):
         self.label1= ttk.Label(master)
-        self.label1.config(text="hello")
+        self.label1.config(text="Hello")
         self.label1.grid()
+
+        self.btn1= ttk.Button(master)
+        self.btn1.config(text= "Press here")
+        self.btn1.config(command= self.GoodbyeMethod)
+        self.btn1.grid()
+        
+    def GoodbyeMethod(self):
+        return self.label1.config(text="Goodbye")
+
 
 app= GreetingApp(root)
 
