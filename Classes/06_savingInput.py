4e47e61cb9eee0dbeb0a1e3efc1a9d3c58a1e6b4
Natalie Russell <natprussell@gmail.com>
2024-03-27 09:38:44 -0500

    add notes

--- Classes/06_savingInput.py
+++ Classes/06_savingInput.py
@@ -4,12 +4,14 @@ root= Tk()
 
 class PracticeClass:
     def __init__(self,master):
-        self.eb1 = ttk.Entry(master)
+        self.eb1 = ttk.Entry(master, show= "*") #hides text being entered
         self.eb1.grid()
         self.submitbutton = ttk.Button(master, text="Submit!",command = self.printinfo)
         self.submitbutton.grid()
     def printinfo(self):
         print(self.eb1.get())
+        self.eb1.delete(0,'end') #deletes entry from console
+
 
 app= PracticeClass(root)
 root.mainloop()
